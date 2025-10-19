d2362f22985cffe517b34983809f1e754900676d
abyan <abyan2006@outlook.com>
2025-10-20 00:37:56 +0200

    endret

---
+++ index.php
@@ -0,0 +1,23 @@
+ndex.php
+
+<!DOCTYPE html>
+<html lang="no">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Obligatorisk oppgave 2 – Programmering</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <h2>Brukerfunksjoner</h2>
+    <ul>
+        <li><a href="registrer-klasse.php" target="_blank">Registrer klasse</a></li>
+        <li><a href="vis-alle-klasser.php" target="_blank">Vis alle klasser</a></li>
+        <li><a href="slett-klasse.php" target="_blank">Slett klasse</a></li>
+        <li><a href="registrer-student.php" target="_blank">Registrer student</a></li>
+        <li><a href="vis-alle-studenter.php" target="_blank">Vis alle studenter</a></li>
+        <li><a href="slett-student.php" target="_blank">Slett student</a></li>
+    </ul>
+</body>
+</html>
